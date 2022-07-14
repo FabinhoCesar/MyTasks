34adc8c2a145fbf44629696f094da0b501a6bac1
Fabio Cesar <fabinhocesar07@gmail.com>
2022-07-14 11:36:59 -0400

    final

--- Projeto_05/index1.js
+++ Projeto_05/index1.js
@@ -15,15 +15,17 @@ let counter = 0
 
 let array = []
 
-const tasks = JSON.parse(localStorage.getItem("Tasks")) || ''
+const tasks = JSON.parse(localStorage.getItem("Tasks")) || []
+
+array = tasks
 
 if (tasks != '') {
     for (let task of tasks) {
-        addTasks(task.id, task.task)
+        addTasks(task.id, task.task, task.class, task.trash)
     }
 }
 
-function save() {
+function save(array) {
     localStorage.setItem("Tasks", JSON.stringify(array))
 }
 
@@ -59,8 +61,8 @@ btnCancel.addEventListener('click', closeModal)
 
 function newTasks(event) {
     event.preventDefault()
-    addTasks(counter, newTask.value)
     arrayTasks(newTask, counter)
+    addTasks(counter, newTask.value)
     closeModal()
     clearInput()
 }
@@ -70,10 +72,11 @@ function newTasks(event) {
 function arrayTasks(newTask, counter) {
     let task = {
         id: counter,
-        task: newTask.value
+        task: newTask.value,
+        class: '',
     }
     array.push(task)
-    save()
+    save(array)
 }
 
 // Criar função para limpar a tarefa inserida pelo usuário
@@ -87,35 +90,32 @@ function clearInput() {
 function addHtml(id, task) {
     const html = `
         <input type="checkbox" name="task${id}" id="task${id}" onclick="searchIndexTasks(this.id)">
-        <label for="task${id}">${task}</label>
-            <div onclick="removeTask(this.id)" id="dask${id}">
-                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0 0 26 26"
-                    style=" fill:#556479;">
-                    <path
-                        d="M 11 -0.03125 C 10.164063 -0.03125 9.34375 0.132813 8.75 0.71875 C 8.15625 1.304688 7.96875 2.136719 7.96875 3 L 4 3 C 3.449219 3 3 3.449219 3 4 L 2 4 L 2 6 L 24 6 L 24 4 L 23 4 C 23 3.449219 22.550781 3 22 3 L 18.03125 3 C 18.03125 2.136719 17.84375 1.304688 17.25 0.71875 C 16.65625 0.132813 15.835938 -0.03125 15 -0.03125 Z M 11 2.03125 L 15 2.03125 C 15.546875 2.03125 15.71875 2.160156 15.78125 2.21875 C 15.84375 2.277344 15.96875 2.441406 15.96875 3 L 10.03125 3 C 10.03125 2.441406 10.15625 2.277344 10.21875 2.21875 C 10.28125 2.160156 10.453125 2.03125 11 2.03125 Z M 4 7 L 4 23 C 4 24.652344 5.347656 26 7 26 L 19 26 C 20.652344 26 22 24.652344 22 23 L 22 7 Z M 8 10 L 10 10 L 10 22 L 8 22 Z M 12 10 L 14 10 L 14 22 L 12 22 Z M 16 10 L 18 10 L 18 22 L 16 22 Z">
-                    </path>
-                </svg>
-            </div>`
+        <label for="task${id}">${task}</label>`
     counter++
     return html
 }
 
 // Criar um elemento na DOM para inserir o html
 
-function createDivTask(id) {
+function createDivTask(id, classe) {
     const element = document.createElement('div')
     element.classList.add('task')
+    if (classe != '') {
+        element.classList.add(classe)
+    }
     element.id = `tasx${id}`
     return element
 }
 
 // Inserir o html dentro da DIV criada e colocar no display
 
-function addTasks(id, task) {
-    const element = createDivTask(id)
+function addTasks(id, task, classe) {
+    const element = createDivTask(id, classe)
     element.innerHTML = addHtml(id, task)
     boxTasksIncomplete.appendChild(element)
     boxTasksIncomplete.insertBefore(element, boxTasksIncomplete.children[0])
+    attDisplayIncomplete()
+    attDisplayCompleted()
 }
 
 // Função para descobrir o index dos inputs para executar as funções de add e remove class
@@ -130,8 +130,22 @@ function searchIndexTasks(nameId) {
             task.classList.toggle('completed')
             attDisplayCompleted()
             attDisplayIncomplete()
+            addClassLocalstorage(nameId)
         }
     }
+}
+
+function addClassLocalstorage(nameId) {
+    nameId = nameId.replace("task", "")
+    console.log(nameId)
+    if (tasks[nameId].class == '') {
+        tasks[nameId].class = 'completed'
+    } else {
+        tasks[nameId].class = ''
+    }
+    
+    console.log(tasks[nameId].class)
+    save(tasks)
 }
 
 
@@ -159,24 +173,9 @@ function attDisplayIncomplete() {
 
 function attDisplayTrash() {
     const trashTask  = document.querySelector('.remove')
+    console.log(trashTask, 'com a classe reomve')
     if (trashTask) {
         boxTrash.appendChild(trashTask)
     }
+    save(tasks)
 }
-
-function removeTask(nameId) {
-    nameId = nameId.replace('d', 't')
-    let allTask = document.querySelectorAll('.task')
-    allTask = Array.from(allTask)
-    for (let task of allTask) {
-        let change = task.id
-        change = change.replace('x', 'k')
-        if (change == nameId) {
-            task.classList.toggle('remove')
-            attDisplayTrash()
-        }
-    }
-
-}
-
-// =============== Tentar fazer local storage
